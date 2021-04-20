890e925338f88a316f4d4d9f75979373f41d1de5
Farah <obaidatfarah4@gmail.com>
2021-04-20 10:56:21 +0300

    first commit

---
+++ numbers.js
@@ -0,0 +1,11 @@
+let a=Number(prompt('enter value',)) || 0
+let b=Number(prompt('enter sec value',)) || 0
+if(a > b){
+    alert("The larger number is a");
+}
+else if (a == b){
+    alert("The numbers are equal");
+}
+else{
+    alert("The larger number is b")
+}
